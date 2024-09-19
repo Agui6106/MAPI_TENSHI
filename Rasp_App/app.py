ef3bc54fc802d098d04a515cee6332bdac24b496
Agui6106 <A01735612@tec.mx>
2024-09-19 19:45:45 +0100

    Agrega comandos para futuras funciones

--- Rasp_App/app.py
+++ Rasp_App/app.py
@@ -7,7 +7,7 @@ import re
 import signal
 import os
 
-# -- FUNCIONES -- #
+# -- FUNCIONES DE CONTROL-- #
 # Función para obtener la IP local
 def get_ip():
     try:
@@ -49,32 +49,50 @@ def detener_proceso(proc):
         print(f"Proceso {proc.pid} detenido.")
     else:
         print("El proceso no está en ejecución.")
+        
+# -- FUNCIONES AUXILIARES PARA EL USARIO -- #
+def help():
+    print(f"stop stream - Detiene el stream de la camara \n")
+    print(f"stop example - Detiene la funcion example \n")
+    print(f"exit - Detiene por compelto la aplicacion \n")
+    
+# -- FUNCIONES DE COMUNICACION ESP32 -- #
+
+# -- FUNCIONES DE COMUNICACION MQTT -- #
 
 # - Aplicacion principal basada en comandos -#
 if __name__ == "__main__":
     # - Inicialización - #
-    print(f"MAPI-Tenshi Robot. Software Version 1.0\nDesarrollado en Santiago de Queretaro, México\n")
+    print(f"MAPI-Tenshi Robot. Software Version 1.0\nDesarrollado en Santiago de Queretaro, México. 2024\n")
     ip = get_ip()
     print(f"IP actual: {ip}")
 
     # Iniciar los procesos
     transmision_proc = iniciar_transmision()
+    mqtt_proc = "MQTT stopped"
+    serial_proc = "Serial stopped"
     ejemplo_proc = iniciar_ejemplo()
     
-    print(f"Consulta el video la direccion url: http://{ip}:8000/index.html")
-
+    print(f"Consulta el video la direccion url: http://{ip}:8000/index.html\n")
+    
     # Interfaz de consola
     while True:
-        comando = input("Escribe 'stop stream', 'stop example' o 'exit' para detener procesos: ")
+        comando = input("Input command : ")
 
         if comando == 'stop stream':
             detener_proceso(transmision_proc)
         elif comando == 'stop example':
             detener_proceso(ejemplo_proc)
+            
+        elif comando == 'stop MQTT':
+            mqtt_proc
+        elif comando == 'stop serial':
+            serial_proc
         elif comando == 'exit':
             print("Saliendo del programa...")
             detener_proceso(transmision_proc)
             detener_proceso(ejemplo_proc)
+            print("Thanks for choosing MAPI software.inc")
             break
         else:
-            print("Comando no reconocido.")
+            print(f"Comando no reconocido. Use help to list all commands.\n")
