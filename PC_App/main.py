ab50d45a2b1c32c88a41a7caddbf2afc71ff3971
unknown <A01735612@tec.mx>
2024-10-01 14:53:40 -0600

    Agrega funciones de cambio de ventana en App_PC

--- PC_App/main.py
+++ PC_App/main.py
@@ -5,55 +5,185 @@ from tkinter.ttk import *
 class App(Frame):
     def __init__(self, parent, *args, **kwargs):       
         Frame.__init__(self, parent, *args, **kwargs)
-        self.parent:Tk = parent
+        self.parent: Tk = parent
         
-        # - Inicializacion de objetos TKinter - #
-        self.title: Button = self._Create_title()
+        # - Creacion de Notebook (pestañas) - #
+        self.notebook = self._Create_notebook()
         
         # - Creacion de los objetos - #
         self.init_gui()
     
     # - Colocamos los elementos visuales - #
-    def init_gui(self,)-> None:
-        self.title.grid(row=0, column=0, columnspan=2,padx=40)
-        
-    # - Atributos y elementos de aplicacion - #
-    # - TITULO - #
-    def _Create_title(self) -> Label:
-        return Label(
-            master = self,
-            text = 'Titulo Ventana Principal',
-            foreground = 'black',
-            font=("Z003",20,"bold")
-        )
+    def init_gui(self)-> None:
+        # -- Propiedades de la App principal -- #
+        self.parent.title('MAPI-Tenshi Control Software - V1.0')
+        # Size del monitor
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        root.geometry(f'{screen_width}x{screen_height}')
 
-# ---- Clase ventana ---- #
-class FrameOne(Frame):
+        # -- Colocacion de widgets -- #
+        self.pack(fill=BOTH, expand=True)
+
+        # Colocamos el Notebook en la ventana
+        self.notebook.pack(fill=BOTH, expand=True)
+        
+    # - Creacion del Notebook con pestañas - #
+    def _Create_notebook(self) -> Notebook:
+        notebook = Notebook(self)
+        
+        # Creamos un frame simple para Tab 1
+        tab1 = Frame(notebook)
+        tab2 = FrameFiles(notebook)
+        tab3 = FrameOptions(notebook)
+        tab4 = FrameWebControl(notebook)
+        tab5 = FrameAbout(notebook)
+        # Agregamos las pestañas al Notebook
+        notebook.add(tab1, text='Main')
+        notebook.add(tab2, text='File')
+        notebook.add(tab3, text='Options')
+        notebook.add(tab4, text='Web Control')
+        notebook.add(tab5, text='About')
+        
+        # - Atributos y elementos de aplicacion - #
+        # - TITULO - #
+        Label(tab1, text="Contenido de la Pestaña 1",
+              foreground='black',
+              font=("Z003", 20, "bold")).grid(row=1, column=0, padx=20, pady=20)
+
+        return notebook
+
+# ---- Clase ventana de archivos ---- #
+class FrameFiles(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
     
         # - Creacion de objetos TKinter - #
         self.title: Label = self._Create_title()
+        self.content: Label = self._content()
         
         # Creamos los objetos
         self.init_gui()
         
     # - Colocamos los elementos visuales - #
-    def init_gui(self,)-> None:
-        self.title.grid(row=0, column=0, columnspan=2,padx=40)
+    def init_gui(self)-> None:
+        self.title.grid(row=0, column=0, columnspan=2, padx=40)
+        
+        # Añadimos un Label en FrameOne usando grid()
+        self.content.grid(row=1, column=0)
         
     # - Atributos y elementos de aplicacion - #
     # - TITULO - #
     def _Create_title(self) -> Label:
         return Label(
-            master = self,
-            text = 'Titulo Ventana Archivo',
-            foreground = 'black',
-            font=("Z003",20,"bold")
+            master=self,
+            text='Titulo Ventana Archivo',
+            foreground='black',
+            font=("Z003", 20, "bold")
         )
     
+    def _content(self) -> Label:
+        return Label(self, text="Contenido de la Pestaña 2")
     
+# ---- Clase ventana de Opciones ---- #
+class FrameOptions(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+    
+        # - Creacion de objetos TKinter - #
+        self.title: Label = self._Create_title()
+        self.content: Label = self._content()
+        
+        # Creamos los objetos
+        self.init_gui()
+        
+    # - Colocamos los elementos visuales - #
+    def init_gui(self)-> None:
+        self.title.grid(row=0, column=0, columnspan=2, padx=40)
+        
+        # Añadimos un Label en FrameOne usando grid()
+        self.content.grid(row=1, column=0)
+        
+    # - Atributos y elementos de aplicacion - #
+    # - TITULO - #
+    def _Create_title(self) -> Label:
+        return Label(
+            master=self,
+            text='Titulo Ventana Opciones',
+            foreground='black',
+            font=("Z003", 20, "bold")
+        )
+    
+    def _content(self) -> Label:
+        return Label(self, text="Contenido de la Pestaña 3")
+    
+# ---- Clase ventana de WebControl ---- #
+class FrameWebControl(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+    
+        # - Creacion de objetos TKinter - #
+        self.title: Label = self._Create_title()
+        self.content: Label = self._content()
+        
+        # Creamos los objetos
+        self.init_gui()
+        
+    # - Colocamos los elementos visuales - #
+    def init_gui(self)-> None:
+        self.title.grid(row=0, column=0, columnspan=2, padx=40)
+        
+        # Añadimos un Label en FrameOne usando grid()
+        self.content.grid(row=1, column=0)
+        
+    # - Atributos y elementos de aplicacion - #
+    # - TITULO - #
+    def _Create_title(self) -> Label:
+        return Label(
+            master=self,
+            text='Titulo Ventana Web Control',
+            foreground='black',
+            font=("Z003", 20, "bold")
+        )
+    
+    def _content(self) -> Label:
+        return Label(self, text="Contenido de la Pestaña 4 (Aqui se abrirar un dcontrol directo MQTT)")
+    
+# ---- Clase ventana de WebControl ---- #
+class FrameAbout(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+    
+        # - Creacion de objetos TKinter - #
+        self.title: Label = self._Create_title()
+        self.content: Label = self._content()
+        
+        # Creamos los objetos
+        self.init_gui()
+        
+    # - Colocamos los elementos visuales - #
+    def init_gui(self)-> None:
+        self.title.grid(row=0, column=0, columnspan=2, padx=40)
+        
+        # Añadimos un Label en FrameOne usando grid()
+        self.content.grid(row=1, column=0)
+        
+    # - Atributos y elementos de aplicacion - #
+    # - TITULO - #
+    def _Create_title(self) -> Label:
+        return Label(
+            master=self,
+            text='About this software',
+            foreground='black',
+            font=("Z003", 20, "bold")
+        )
+    
+    def _content(self) -> Label:
+        return Label(self, text="Runing App Stable Version 0.2")
 # ------------------------------------------------------ #
 # -------------- Inicializacion de la app -------------- #
 # ------------------------------------------------------ #
