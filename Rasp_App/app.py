1629ef2ac122ce7023819303a29aabaab0d020d7
Agui6106 <A01735612@tec.mx>
2024-10-01 23:16:16 +0100

    Reestructuracion de aplicacion de comando para Raspberry

--- Rasp_App/app.py
+++ Rasp_App/app.py
@@ -7,10 +7,17 @@ import re
 import signal
 import os
 
-#print(utils)
+# -- FUNCIONES DE PROTOCOLO DE INICIO-- #
+"""
+    SECUENCIA DE INICIALZIACION 
+    * 1) Bienvenida e IP actual
+    * 2) Inicialzar MQTT (Control de la app por comandos)
+    * 3) Conexion serial a ESP32 por puerto USB (Pract. Arduino)
+    * 4) Iniciar servidor de camara
+    * S) Ya que el usuario lo ordene por MQTT - PARAR
+"""
 
-# -- FUNCIONES DE CONTROL-- #
-# Función para obtener la IP local
+# 1) Función para obtener la IP local
 def get_ip():
     try:
         resultado = subprocess.check_output(["ifconfig"], encoding='utf-8')
@@ -19,83 +26,93 @@ def get_ip():
         if ip_local:
             return ip_local[0]
         else:
-            return "No se pudo encontrar la IP local."
+            return "Cant obtain actual ip"
     except subprocess.CalledProcessError as e:
-        return f"Error al ejecutar el comando: {e}"
-
-# Función para iniciar el proceso de streaming
+        return f"Error executing: {e}"
+    
+# 2) Función para iniciar el proceso de comunicacion MQTT
+def iniciar_Mqtt():
+    print("Starting Mqtt protocol...")
+    try:
+        proc = subprocess.Popen(["python3", "./mqtt.py"])
+        return proc
+    except Exception as e:
+        print(f'Failed to start MQTT protocol due to: {str(e)}')
+        return None
+    
+# 3)  Conexion serial a ESP32 por puerto USB 
+def iniciar_Serial():
+    print("Starting serial comunications...")
+    try:
+        proc = subprocess.Popen(["python3", "./start_serial_com.py"])
+        return proc
+    except Exception as e:
+        print(f'Failed to start communication due to: {str(e)}')
+        return None
+    
+# 4) Función para iniciar el proceso de streaming
 def iniciar_transmision():
-    print("Inicializando transmisión de la cámara...")
+    print("Starting camera stream...\n")
     try:
         proc = subprocess.Popen(["python3", "./stream.py"])
-        print(f'Transmision iniciada con exito\n')
+        print(f'Transmison started succesfully\n')
         return proc
     except Exception as e:
-        print(f'Failed to start transmission: {str(e)}')
+        print(f'Failed to start transmission due to: {str(e)}')
         return None
 
-# Función para iniciar el proceso de ...
-def iniciar_ejemplo():
-    print("Inicializando ejemplo...")
-    try:
-        proc = subprocess.Popen(["python3", "./example.py"])
-        return proc
-    except Exception as e:
-        print(f'Failed to start example: {str(e)}')
-        return None
-
-# Función para detener un proceso dado su objeto de proceso
+# S) Función para detener un proceso dado su objeto de proceso
 def detener_proceso(proc):
     if proc:
         os.kill(proc.pid, signal.SIGTERM)
-        print(f"Proceso {proc.pid} detenido.")
+        print(f"Process {proc.pid} succesfully stopped.")
     else:
-        print("El proceso no está en ejecución.")
+        print(f"The process {proc.pid} is not runnig or failed to stop")
         
 # -- FUNCIONES AUXILIARES PARA EL USARIO -- #
 def help():
     print(f"stop stream - Detiene el stream de la camara \n")
     print(f"stop example - Detiene la funcion example \n")
     print(f"exit - Detiene por compelto la aplicacion \n")
-    
-# -- FUNCIONES DE COMUNICACION ESP32 -- #
-
-# -- FUNCIONES DE COMUNICACION MQTT -- #
 
 # - Aplicacion principal basada en comandos -#
 if __name__ == "__main__":
     # - Inicialización - #
     print(f"MAPI-Tenshi Robot. Software Version 1.0\nDesarrollado en Santiago de Queretaro, México. 2024\n")
     ip = get_ip()
-    print(f"IP actual: {ip}")
+    print(f"Actual IP: {ip}")
 
-    #connect_serial_device()
-    # Iniciar los procesos
+    # -- SECUENCIA DE INICIALZIACION -- #
+    mqtt_proc = iniciar_Mqtt()
+    serial_proc = iniciar_Serial()
     transmision_proc = iniciar_transmision()
-    mqtt_proc = "MQTT stopped"
-    serial_proc = "Serial stopped"
-    ejemplo_proc = iniciar_ejemplo()
+
+    print(f"Video stream available in url: http://{ip}:8000/index.html\n")
     
-    print(f"Consulta el video la direccion url: http://{ip}:8000/index.html\n")
-    
-    # Interfaz de consola
+    # - Interfaz de consola - #
     while True:
         comando = input("Input command: ")
 
-        if comando == 'stop stream':
-            detener_proceso(transmision_proc)
-        elif comando == 'stop example':
-            detener_proceso(ejemplo_proc)
-            
-        elif comando == 'stop MQTT':
-            mqtt_proc
+        # - Comandos principales - #
+        if comando == 'stop MQTT':
+            detener_proceso(mqtt_proc)
         elif comando == 'stop serial':
-            serial_proc
-        elif comando == 'exit':
-            print("Saliendo del programa...")
+            detener_proceso(serial_proc)
+        elif comando == 'stop transmision':
             detener_proceso(transmision_proc)
-            detener_proceso(ejemplo_proc)
-            print("Thanks for choosing MAPI software.inc")
+            
+        # - Comandos auxilaires - #
+        elif comando == "help":
+            help()
+        
+        # - Comando de salida - #
+        elif comando == 'exit':
+            print("Leaving program...")
+            detener_proceso(mqtt_proc)
+            detener_proceso(serial_proc)
+            detener_proceso(transmision_proc)
+        
+            print("\nThanks for choosing MAPI software.inc")
             break
         else:
-            print(f"Comando no reconocido. Use help to list all commands.\n")
+            print(f"Command not found. Use help to list all commands.\n")
