f7cc17470fd21e9e00d38023898369834ea2a5b6
unknown <A01735612@tec.mx>
2024-10-11 22:23:31 -0600

    Agrega funciones de lectura de control (D-Pad)

--- PC_App/controls/ps4_control.py
+++ PC_App/controls/ps4_control.py
@@ -1,42 +1,106 @@
 import pygame
 
-def get_available_controllers():
-    pygame.init()
-    pygame.joystick.init()
-    
-    controllers = []
-    
-    # Obtener la cantidad de joysticks conectados
-    joystick_count = pygame.joystick.get_count()
-    
-    if joystick_count == 0:
-        print("No hay controles conectados")
-    else:
-        print(f"Se detectaron {joystick_count} controles")
-        
-        for i in range(joystick_count):
-            joystick = pygame.joystick.Joystick(i)
-            joystick.init()
+pygame.init()
+pygame.joystick.init()
+
+#joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+# - BOTONES Y JOYSTICKS - #
+def get_buttons():
+    """
+    Nos regresa todos los botones del control
+    """
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 0:
+                    return 'X'
+
+                if event.button == 1:
+                    return 'O'
+
+                if event.button == 2:
+                    return 'SQR'
+
+                if event.button == 3:
+                    return 'TRI'
+                
+                if event.button == 11:
+                    return 'D_UP'
+                
+                if event.button == 12:
+                    return 'D_DWN'
+                
+                if event.button == 13:
+                    return 'D_LFT'
+                
+                if event.button == 14:
+                    return 'D_RGT'
+
+def get_joys_left():
+    """
+    Nos regresa las posiciones de la palanca Izquierda
+    """
+    x = 0
+    y = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYAXISMOTION:
+                x = pygame.joystick.Joystick(0).get_axis(0)
+                y = pygame.joystick.Joystick(0).get_axis(1)
+                return x, y
             
-    return controllers
+def get_joys_right():
+    """
+    Nos regresa las posiciones de la palanca Derecha
+    """
+    x = 0
+    y = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.JOYAXISMOTION:
+                x = pygame.joystick.Joystick(0).get_axis(2)
+                y = pygame.joystick.Joystick(0).get_axis(3)
+                return x, y
 
+# - INFORMACION DEL PAD - #
+def get_pad_info(id_pad, val):
+    """
+    Obtener la informacion del control.
+    val = name, id, power, buttons, axes
+    """
+    joystick = pygame.joystick.Joystick(id_pad)
 
-def get_control_info(controller_id):
-    if controller_id < pygame.joystick.get_count():
-        joystick = pygame.joystick.Joystick(controller_id)
-        joystick.init()
-        
-        controller_info = {
-            "id": controller_id,
-            "name": joystick.get_name(),
-            "num_axes": joystick.get_numaxes(),
-            "num_buttons": joystick.get_numbuttons(),
-            "num_hats": joystick.get_numhats(),
-        }
-        return controller_info
-    else:
-        print(f"No existe control con ID {controller_id}")
-        return None
+    if val == 'name':
+        return joystick.get_name()
 
-# Llamar a la función y mostrar los controles disponibles
-available_controllers = get_available_controllers()
+    if val == 'id':
+        return joystick.get_id()
+    
+    if val == 'power':
+        return joystick.get_power_level()
+    
+    if val == 'buttons':
+        return joystick.get_numbuttons()
+    
+    if val == 'axes':
+        return joystick.get_numaxes()       
+                    
+if __name__ == "__main__":
+    
+    # - Info - #
+    print(f'\nName: {get_pad_info(0,'name')}')
+    print(f'Id: {get_pad_info(0,'id')}')
+    print(f'Power: {get_pad_info(0,'power')}')
+    print(f'Total Buttons: {get_pad_info(0,'buttons')}')
+    print(f'Total Axes: {get_pad_info(0,'axes')}')
+    
+    # - Butons - #
+    while True:
+        button = get_buttons()
+        xval_left, yval_left = get_joys_left()
+        xval_right, yval_right = get_joys_right()
+        print(button)
+        print('')
+        print(f'X Left: {xval_left}, Y Left: {yval_left} \n')
+        print(f'X right: {xval_right}, Y right: {yval_right} \n')
