ecc96b049ff325e7394314a80a128b311fdf741a
Agui6106 <A01735612@tec.mx>
2024-11-06 18:58:58 +0000

    Optimiza funciones de conexiones seriales

--- Rasp_App/SerialCom/serial_sensor.py
+++ Rasp_App/SerialCom/serial_sensor.py
@@ -1,9 +1,10 @@
-#Librerias standard
 import time
-#Librerias de 3ros
 import serial
 
-#Velocidad a la que se envian o reciben los caracteres
+import sys
+import glob
+
+# Avaibale Baudrates
 BAUDRATES = [  
     2400,
     4800,
@@ -14,6 +15,32 @@ BAUDRATES = [
     57600, 
     115200
 ]
+
+def find_available_serial_ports() -> list[str]:
+    if sys.platform.startswith('win'): # Computadora windows
+        platform = 'win'
+        ports =[f'COM{i}' for i in range(1, 256)]
+    elif sys.platform.startswith('linux'): # Computadora Linux
+        platform = 'linux'
+        ports =glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'): # Mac
+        platform = 'darwin'
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported Platform')
+    result = []
+    for port in ports:
+        early_stop = True if platform == 'win' else False
+        try:
+            s= serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            if early_stop:
+                break
+            continue
+
+    return result
 
 class SerialSensor:
     
