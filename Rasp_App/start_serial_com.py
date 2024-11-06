e60f501e3bd60156a26f2dafe8c1101d7425353b
Agui6106 <A01735612@tec.mx>
2024-11-06 22:58:19 +0000

    Agrega funciones de serial

--- Rasp_App/start_serial_com.py
+++ Rasp_App/start_serial_com.py
@@ -20,19 +20,11 @@ def connect_serial_device() -> None:
         # Verificar si el dispositivo serial está abierto
         if serial_device.is_open():
             print(f'Connection Successful... Device connected on {port} with baudrate {baudrate}')
+            return serial_device
         else:
             print('Failed to open serial device.')
             return
-
-        # Comunicación constante hasta interrupción manual
-        while True:
-            if serial_device.in_waiting() > 0:
-                data_received = serial_device.reception()
-                print(f"Data received: {data_received}")
-
-                # Ejemplo de respuesta: envía "ACK" al dispositivo
-                serial_device.send("ACK")
-
+        
     except ValueError:
         print('Invalid baudrate specified')
     except KeyboardInterrupt:
@@ -41,6 +33,40 @@ def connect_serial_device() -> None:
         if 'serial_device' in locals() and serial_device.is_open():
             serial_device.close()
             print("Serial device closed.")
+  
+def read_serial(device):
+    if device.in_waiting > 0:
+        try:
+            data = device.readline().decode().strip()
+            return data
+        except:
+            return None
+    return None
+
+# Comunicación constante hasta interrupción manual
+"""
+while True:
+    if serial_device.in_waiting() > 0:
+        data_received = serial_device.reception()
+        print(f"Data received: {data_received}"
+        # Ejemplo de respuesta: envía "ACK" al dispositivo
+        serial_device.send("ACK")
+"""
+        
+
 
 if __name__ == "__main__":
-    connect_serial_device()
+    device = connect_serial_device()
+    # Comunicación constante hasta interrupción manual
+    while True:
+        # Solicitar entrada del usuario para enviar un mensaje
+        user_input = input("Enter message to send to Arduino (or type 'exit' to quit): ")
+
+        if user_input.lower() == 'exit':
+            print("Exiting communication...")
+            break  # Salir del bucle si el usuario escribe 'exit'
+        elif user_input:
+            # Enviar el mensaje ingresado por el usuario
+            device.send(user_input)
+            print(f"Sent to Arduino: {user_input}")
+            print(f'Arduino Response: {command}')
